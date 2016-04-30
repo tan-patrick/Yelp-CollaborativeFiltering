75148a0bc0c0e9a19d800aa44ba08fbb6f89a3d7
cterndrup <cterndrup@cs.ucla.edu>
2016-04-29 20:08:09 -0700

    add data preprocessing scripts

--- Dataset/filter_restaurants.py
+++ Dataset/filter_restaurants.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python
 # python script to extract restaurants from yelp business data and write to new json file
+# done for preprocessing of data
 import json
 
 if __name__ == "__main__":
 
 	# open yelp business data file and restaurant data file
 	restaurants_file = "restaurants.json"
-	biz_data_fp = open("yelp_academic_dataset_business.json", 'r')
+	biz_data_file = "./yelp_dataset_challenge_academic_dataset/yelp_academic_dataset_business.json"
+	biz_data_fp = open(biz_data_file, 'r')
 	restaurant_data_fp = open(restaurants_file, 'w')
 
 	# write all restaurants in business data file to restaurant file
---
+++ Dataset/filter_reviews.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python
+# extract all restaurant reviews from all business reviews and write to reviews.json file
+# done for preprocessing
+import json
+import restaurants
+
+# get list of restaurants, build restaurant_id-->name dict
+# iterate through list of reviews, checking if corresponding
+# business_id is a restaurant
+# if so, write json back to reviews.json file
+
+if __name__ == '__main__':
+	# get all restaurants from restaurants.json file
+	restaurants = restaurants.Restaurants("restaurants.json")
+	
+	restaurant_dict = dict()
+	
+	# build business_id-->restaurant_name dictionary for quick lookup
+	for r in restaurants.list_restaurants():
+		restaurant_dict[r.business_id()] = r.name()
+	
+	reviews_fp = open("reviews.json", 'w')
+
+	# iterate through all reviews and only write restaurant reviews to "reviews.json"	
+	reviews_data_file = "./yelp_dataset_challenge_academic_dataset/yelp_academic_dataset_review.json"
+	with open(reviews_data_file, 'r') as rf:
+		for entry in rf:
+			review = json.loads(entry)
+			if restaurant_dict.get(review['business_id']):
+				reviews_fp.write(entry)
+	
+	reviews_fp.close()
